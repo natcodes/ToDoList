081b1c3a5ff8a5ee2d3659f711c7cb561cedb538
Natalie <natalienuno@gmail.com>
2018-03-23 20:04:36 -0700

    end of day exam day

--- TodoList/TodoListTableTableViewController.swift
+++ TodoList/TodoListTableTableViewController.swift
@@ -14,12 +14,22 @@ class TodoListTableViewController: UITableViewController, AddItemTVCDelegate {
     var todo = [TodoList]()
     var managedObjectContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
+    override func viewDidLoad() { //if 0 items in the db then custom cells wont render.
+        super.viewDidLoad()
+        fetchAll()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        fetchAll()
+        tableView.reloadData()
+    }
     
     func cancelButtonPressed(by controller: AddViewController) {
         dismiss(animated: true, completion: nil)
     }
     
     // ===============  CRUD ========================================
+                    // CREATE
     func itemSaved(by controller: AddViewController, with task: String, date: Date, desc: String, at indexPath: NSIndexPath?) {
         if let ip = indexPath {
             let addition = todo[ip.row]
@@ -39,45 +49,55 @@ class TodoListTableViewController: UITableViewController, AddItemTVCDelegate {
         }catch{
             print("\(error)")
         }
-        tableView.reloadData()
         dismiss(animated: true, completion: nil)
     }
+                    // REMOVE
     
     
     
+                        // UPDATE
     
     @IBOutlet var todoListTableView: UITableView!
  
-    override func viewDidLoad() { //if 0 items in the db then custom cells wont render.
-        super.viewDidLoad()
+ 
+    // ========   FETCH ALL  ====================
+    
+    func fetchAll() {
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "TodoList")
         do {
-            let list = try managedObjectContext.fetch(TodoList.fetchRequest())
-            todo = list as! [TodoList]
+            let results = try managedObjectContext.fetch(request)
+            todo = results as! [TodoList]
         } catch {
-            print("Error: \(error)")
+            print("\(error)")
         }
     }
-
-}
     
+    // PREPARE HANDSHAKE
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let navController = segue.destination as! UINavigationController
+        let addController = navController.topViewController as! AddViewController
+        addController .delegate = self
+        
+//        if let indexPath = sender as? NSIndexPath {
+//            let todos = todo[indexPath.row]
+        
+    }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! Cell
         let todoItem = todo[indexPath.row]
-        cell.textLabel!.text = todoItem.task
+        cell.taskLabel.text = todoItem.task
+        let dateFormatter = DateFormatter()
+        dateFormatter.timeStyle = .none
+        dateFormatter.locale = Locale(identifier: "en_US")
+        dateFormatter.dateFormat = "dd/MM/yyyy"
+        let formattedDate = dateFormatter.string(from: todoItem.date!)
+        print(formattedDate)
+        cell.dateLabel.text = formattedDate
+        cell.descLabel.text = todoItem.desc
+        
+        
 //        if todoItem.containsObject(self.todo.objectAtIndex(indexPath.row) as! String)
 //        {
 //            cell.accessoryType = .checkmark
